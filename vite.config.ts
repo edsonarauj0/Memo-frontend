6f497a5ed31eec061cecdb966916369c37df571b
Edson Araújo <edsonaraujo.dev@gmail.com>
2025-09-12 19:53:02 -0300

    fix: configure vite alias for src

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from "path";
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/tests/setup.ts',
   },
-  alias: {
-      "@": path.resolve(__dirname, "src")
-    }
 })
